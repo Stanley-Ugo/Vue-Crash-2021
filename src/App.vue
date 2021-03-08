08b0d86203408a904810de999f8b05a4ca983bb6
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-03-08 19:25:36 +0100

    Catching the emit in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks"/>
+    <Tasks @delete-task="deleteTask" :tasks="tasks"/>
   </div>
   
 </template>
@@ -15,6 +15,11 @@ export default {
   components: {
     Header,
     Tasks
+  },
+  methods: {
+    deleteTask(id){
+      console.log('task', id)
+    }
   },
   data() {
     return {
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -14,6 +14,7 @@ import Task from './Task'
       },
       components: {
         Task
-      }
+      },
+      emits: ['delete-task'],
   }
 </script>
