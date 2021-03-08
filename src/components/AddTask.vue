ac2d721216c4ce61f41974ae1b1278a663667dbb
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-03-09 00:55:52 +0100

    Adding the onSubmit() on AddTask.vue

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -27,7 +27,32 @@
       name: 'AddTask',
       data() {
           return {
-              
+              text: '',
+              day: '',
+              reminder: false,
+          }
+      },
+      methods: {
+          onSubmit(e) {
+              e.preventDefault()
+
+              if(!this.text){
+                  alert('Please Add A Task!!')
+                  return
+              }
+
+              const newTask = {
+                  id: Math.floor(Math.random() * 100000),
+                  text: this.text,
+                  day: this.day,
+                  reminder: this.reminder
+              }
+
+              console.log(newTask)
+
+              this.text = '',
+              this.day = '',
+              this.reminder = false
           }
       }
   }
